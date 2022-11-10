97ba2dad8c850d9df68606063d8060a1861e375c
JeanBaptisteRAGUES <jeanbonduel313@gmail.com>
2022-11-10 03:56:03 +0100

    Création d'une TODO app utilisant les nouvelles fonctionnalités offertes par Next.js 13

---
+++ app/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className=' p-5 bg-blue-500' >
+        <p className=' font-bold text-white'>
+            I am a header
+        </p>
+    </header>
+  )
+}
+
+export default Header;
