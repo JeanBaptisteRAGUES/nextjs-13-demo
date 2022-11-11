0e1d5a0f2d75a2c173549da2e34407febc8e414f
JeanBaptisteRAGUES <jeanbonduel313@gmail.com>
2022-11-11 03:55:08 +0100

    Création du composant loading.tsx qui s'affiche à chaque fois qu'un évènement bloquant est en attente (ex : requête api avec await).

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+//Créé avec : npx create-next-app -e with-tailwindcss nom_du_projet
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
